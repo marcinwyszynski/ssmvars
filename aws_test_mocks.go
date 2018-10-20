fc6dc9fde7dcfc8f8959b0c5a322933583b584f8
Marcin Wyszynski <marcin.pixie@gmail.com>
2018-10-20 17:44:17 +0200

    Add reset method (#6)

--- aws_test_mocks.go
+++ aws_test_mocks.go
@@ -15,6 +15,21 @@ type mockSSMAPI struct {
 	parameters []*ssm.Parameter
 }
 
+func (m *mockSSMAPI) DeleteParameterWithContext(ctx aws.Context, input *ssm.DeleteParameterInput, opts ...request.Option) (*ssm.DeleteParameterOutput, error) {
+	args := m.Called(ctx, input, opts)
+	return args.Get(0).(*ssm.DeleteParameterOutput), args.Error(1)
+}
+
+func (m *mockSSMAPI) DeleteParametersWithContext(ctx aws.Context, input *ssm.DeleteParametersInput, opts ...request.Option) (*ssm.DeleteParametersOutput, error) {
+	args := m.Called(ctx, input, opts)
+	return args.Get(0).(*ssm.DeleteParametersOutput), args.Error(1)
+}
+
+func (m *mockSSMAPI) GetParameterWithContext(ctx aws.Context, input *ssm.GetParameterInput, opts ...request.Option) (*ssm.GetParameterOutput, error) {
+	args := m.Called(ctx, input, opts)
+	return args.Get(0).(*ssm.GetParameterOutput), args.Error(1)
+}
+
 func (m *mockSSMAPI) GetParametersByPathPagesWithContext(ctx aws.Context, input *ssm.GetParametersByPathInput, cursor func(*ssm.GetParametersByPathOutput, bool) bool, opts ...request.Option) error {
 	args := m.Called(ctx, input, cursor, opts)
 	cursor(&ssm.GetParametersByPathOutput{Parameters: m.parameters}, true)
@@ -24,14 +39,4 @@ func (m *mockSSMAPI) GetParametersByPathPagesWithContext(ctx aws.Context, input 
 func (m *mockSSMAPI) PutParameterWithContext(ctx aws.Context, input *ssm.PutParameterInput, opts ...request.Option) (*ssm.PutParameterOutput, error) {
 	args := m.Called(ctx, input, opts)
 	return args.Get(0).(*ssm.PutParameterOutput), args.Error(1)
-}
-
-func (m *mockSSMAPI) DeleteParameterWithContext(ctx aws.Context, input *ssm.DeleteParameterInput, opts ...request.Option) (*ssm.DeleteParameterOutput, error) {
-	args := m.Called(ctx, input, opts)
-	return args.Get(0).(*ssm.DeleteParameterOutput), args.Error(1)
-}
-
-func (m *mockSSMAPI) GetParameterWithContext(ctx aws.Context, input *ssm.GetParameterInput, opts ...request.Option) (*ssm.GetParameterOutput, error) {
-	args := m.Called(ctx, input, opts)
-	return args.Get(0).(*ssm.GetParameterOutput), args.Error(1)
 }
